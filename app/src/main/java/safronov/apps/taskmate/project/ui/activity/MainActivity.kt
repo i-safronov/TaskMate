1fa158619fd29151957b29339e4d7fb4621ab6e4
safronov <ilya.safa1337w@gmail.com>
2023-10-18 09:23:38 +0500

    Added TestKeys

--- app/src/main/java/safronov/apps/taskmate/project/ui/activity/MainActivity.kt
+++ app/src/main/java/safronov/apps/taskmate/project/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package safronov.apps.taskmate
+package safronov.apps.taskmate.project.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import safronov.apps.taskmate.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
