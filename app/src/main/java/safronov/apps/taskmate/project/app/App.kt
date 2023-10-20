804ae256d7cbe2ab69e7084be684f32c47c46689
safronov <ilya.safa1337w@gmail.com>
2023-10-20 10:23:17 +0500

    Added App.kt

---
+++ app/src/main/java/safronov/apps/taskmate/project/app/App.kt
@@ -0,0 +1,17 @@
+package safronov.apps.taskmate.project.app
+
+import android.app.Application
+import safronov.apps.taskmate.project.di.component.AppComponent
+import safronov.apps.taskmate.project.di.component.DaggerAppComponent
+import safronov.apps.taskmate.project.di.module.AppModule
+
+class App: Application() {
+
+    private lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.builder().appModule(AppModule(context = this@App)).build()
+    }
+
+}
--- app/src/main/java/safronov/apps/taskmate/project/di/component/AppComponent.kt
+++ app/src/main/java/safronov/apps/taskmate/project/di/component/AppComponent.kt
@@ -1,7 +1,10 @@
 package safronov.apps.taskmate.project.di.component
 
 import dagger.Component
+import safronov.apps.taskmate.project.di.module.AppModule
+import safronov.apps.taskmate.project.di.module.DataModule
+import safronov.apps.taskmate.project.di.module.DomainModule
 
-@Component
+@Component(modules = [AppModule::class, DomainModule::class, DataModule::class])
 interface AppComponent {
 }
