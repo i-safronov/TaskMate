b17e5728282d69d03cda75c36f7118ff3ccf305c
safronov <ilya.safa1337w@gmail.com>
2023-10-21 12:37:51 +0300

    Added unit tests in the FragmentWelcomeTest

--- app/src/androidTest/java/safronov/apps/taskmate/project/ui/fragment/welcome/FragmentWelcomeTest.kt
+++ app/src/androidTest/java/safronov/apps/taskmate/project/ui/fragment/welcome/FragmentWelcomeTest.kt
@@ -1,6 +1,7 @@
 package safronov.apps.taskmate.project.ui.fragment.welcome
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -21,6 +22,27 @@ class FragmentWelcomeTest {
 
     @Test
     fun testButtonContinueIsVisible() {
+        onView(withId(R.id.btn_sign_in)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testPrivacyPoliceIsVisible() {
+        onView(withId(R.id.tv_privacy_police)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testWelcomeImgIsVisible() {
+        onView(withId(R.id.welcome_img)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testButtonContinueClick_shouldShowProgressBarAndAnotherText() {
+        onView(withId(R.id.btn_sign_in)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_sign_in)).perform(click()).check(matches(isDisplayed()))
+        onView(withId(R.id.loaging_progress_bar)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_please_wait)).check(matches(isDisplayed()))
+        onView(withId(R.id.welcome_img)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_privacy_police)).check(matches(isDisplayed()))
         onView(withId(R.id.btn_sign_in)).check(matches(isDisplayed()))
     }
 
