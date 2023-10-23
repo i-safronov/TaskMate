21265488f8b2d3e741c90e5ec103d3424dbb6a92
safronov <ilya.safa1337w@gmail.com>
2023-10-23 17:03:02 +0500

    Added unit tests in the FragmentMainTest

--- app/src/androidTest/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/FragmentMainTest.kt
+++ app/src/androidTest/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/FragmentMainTest.kt
@@ -1,8 +1,10 @@
 package safronov.apps.taskmate.project.ui.fragment.fragment_main
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -27,6 +29,40 @@ class FragmentMainTest {
         onView(withId(R.id.included_search_view)).check(matches(not(isDisplayed())))
     }
 
-    //TODO write tests for event when user click on button, choose task category an etc
+    @Test
+    fun test_clickOnFbAddTask_shouldShowBottomSheetAndChooseTaskType() {
+        onView(withId(R.id.fb_add_task)).check(matches(isDisplayed()))
+        onView(withId(R.id.fb_add_task)).perform(click()).check(matches(isDisplayed()))
+        onView(withId(R.id.choose_task_type_layout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_clickOnFbAddTaskAndClickOnFirstTaskType_shouldOpenFragmentForCreatingTask() {
+        onView(withId(R.id.fb_add_task)).check(matches(isDisplayed()))
+        onView(withId(R.id.fb_add_task)).perform(click()).check(matches(isDisplayed()))
+        onView(withId(R.id.choose_task_type_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.rcv_tasks_type)).check(matches(isDisplayed()))
+        onView(withId(R.id.rcv_tasks_type))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RcvTaskType.TaskTypeViewHolder>(
+                0, click()
+            ))
+        onView(withId(R.id.choose_task_type_layout)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun test_clickOnFbAddTaskAndClickOnFirstTaskTypeAndAfterGoBack() {
+        onView(withId(R.id.choose_task_type_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.rcv_tasks_type)).check(matches(isDisplayed()))
+        onView(withId(R.id.rcv_tasks_type))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RcvTaskType.TaskTypeViewHolder>(
+                0, click()
+            ))
+        onView(withId(R.id.choose_task_type_layout)).check(matches(not(isDisplayed())))
+
+        pressBack()
+
+        onView(withId(R.id.choose_task_type_layout)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.rcv_tasks_type)).check(matches(not(isDisplayed())))
+    }
 
 }
