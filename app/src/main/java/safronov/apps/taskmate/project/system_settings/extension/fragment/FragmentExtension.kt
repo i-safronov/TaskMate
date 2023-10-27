f634b8570297c3ff741aea138c7d476295a50b6f
safronov <ilya.safa1337w@gmail.com>
2023-10-27 17:23:11 +0500

    Changed focusOnViewAndShowKeyboard method in the FragmentExtension.kt

--- app/src/main/java/safronov/apps/taskmate/project/system_settings/extension/fragment/FragmentExtension.kt
+++ app/src/main/java/safronov/apps/taskmate/project/system_settings/extension/fragment/FragmentExtension.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
@@ -56,10 +57,21 @@ fun Fragment.requireHomePageToolBar(): Toolbar {
     return requireActivity().findViewById(R.id.included_fragment_home_page_toolbar)
 }
 
-fun Fragment.focusOnViewAndShowKeyboard(view: View) {
-    view.requestFocus()
+fun Fragment.focusOnViewAndShowKeyboard(currentView: SearchView) {
+    currentView.requestFocus()
     val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
-    imm?.showSoftInput(view, InputMethodManager.SHOW_FORCED)
+    imm?.showSoftInput(currentView, InputMethodManager.SHOW_FORCED)
+    currentView.requestFocus()
+    currentView.setOnQueryTextFocusChangeListener { view, hasFocus ->
+        if (hasFocus) {
+            showInputMethod(view.findFocus())
+        }
+    }
+}
+
+fun Fragment.showInputMethod(view: View) {
+    val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+    imm?.showSoftInput(view, 0)
 }
 
 fun Fragment.inflateMenuOnHomePageToolBar(
