bab7f52b0328e5abbeb3519f45f7b422b0f17d25
safronov <ilya.safa1337w@gmail.com>
2023-10-30 11:17:28 +0500

    Added TaskEntityConverter.kt and changed TaskEntity.kt

---
+++ data/src/main/java/safronov/apps/data/data_source/local/model/converter/task/TaskEntityConverter.kt
@@ -0,0 +1,13 @@
+package safronov.apps.data.data_source.local.model.converter.task
+
+import safronov.apps.data.data_source.local.model.task.TaskEntity
+import safronov.apps.domain.model.task.Task
+
+interface TaskEntityConverter {
+
+    fun convertTaskEntityToTaskList(taskEntity: TaskEntity): Task.TaskList
+    fun convertTaskEntityToTaskText(taskEntity: TaskEntity): Task.TaskText
+    fun convertListOfTaskEntityToListOfTaskList(list: List<TaskEntity>): List<Task.TaskList>
+    fun convertListOfTaskEntityToListOfTaskText(list: List<TaskEntity>): List<Task.TaskText>
+
+}
--- data/src/main/java/safronov/apps/data/data_source/local/model/task/TaskEntity.kt
+++ data/src/main/java/safronov/apps/data/data_source/local/model/task/TaskEntity.kt
@@ -3,6 +3,7 @@ package safronov.apps.data.data_source.local.model.task
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.Gson
 import safronov.apps.domain.model.task.Task
 
 @Entity(tableName = TaskEntity.TASK_ENTITY_TABLE_NAME)
