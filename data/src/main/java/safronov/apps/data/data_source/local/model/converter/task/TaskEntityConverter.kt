447c636be8e1a24c1ee9dc676a9feaf81abe95eb
safronov <ilya.safa1337w@gmail.com>
2023-10-30 12:28:48 +0500

    Added unit tests in the TaskEntityConverterTest.kt and added more methods in the TaskEntityConverter.kt

--- data/src/main/java/safronov/apps/data/data_source/local/model/converter/task/TaskEntityConverter.kt
+++ data/src/main/java/safronov/apps/data/data_source/local/model/converter/task/TaskEntityConverter.kt
@@ -7,9 +7,14 @@ interface TaskEntityConverter {
 
     fun convertTaskEntityToTaskList(taskEntity: TaskEntity): Task.TaskList
     fun convertTaskEntityToTaskText(taskEntity: TaskEntity): Task.TaskText
+
     fun convertTaskTextToTaskEntity(task: Task.TaskText): TaskEntity
     fun convertTaskListToTaskEntity(task: Task.TaskList): TaskEntity
+
     fun convertListOfTaskEntityToListOfTaskList(list: List<TaskEntity>): List<Task.TaskList>
     fun convertListOfTaskEntityToListOfTaskText(list: List<TaskEntity>): List<Task.TaskText>
+
+    fun convertListOfTaskTextToListOfTaskEntity(list: List<Task.TaskText>): List<TaskEntity>
+    fun convertListOfTaskListToListOfTaskEntity(list: List<Task.TaskList>): List<TaskEntity>
 
 }
--- data/src/test/java/safronov/apps/data/data_source/local/model/converter/task/TaskEntityConverterTest.kt
+++ data/src/test/java/safronov/apps/data/data_source/local/model/converter/task/TaskEntityConverterTest.kt
@@ -1,17 +1,179 @@
 package safronov.apps.data.data_source.local.model.converter.task
 
-/*
-*
-* 1. конвертировать из энтити в бизнес текст
-* 2. конвертировать из энтити в бизнес список
-* 3. конвертировать из бизнес список в энтити
-* 4. конвертировать из бизнес текст в энтити
-* 5. конвертировать из списка энтити в список бизнес текст
-* 6. конвертировать из списка энтити в список бизнес списка
-*
-* */
+import com.google.gson.Gson
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import safronov.apps.data.data_source.local.model.task.TaskEntity
+import safronov.apps.domain.model.task.Task
 
 class TaskEntityConverterTest {
 
+    private lateinit var taskEntityText: TaskEntity
+    private lateinit var taskEntityList: TaskEntity
+    private lateinit var taskText: Task.TaskText
+    private lateinit var taskList: Task.TaskList
+    private lateinit var listOfTaskEntityText: List<TaskEntity>
+    private lateinit var listOfTaskEntityList: List<TaskEntity>
+    private lateinit var listOfTaskText: List<Task.TaskText>
+    private lateinit var listOfTaskList: List<Task.TaskList>
+    private lateinit var taskEntityConverter: TaskEntityConverter
+
+    @Before
+    fun setUp() {
+        taskEntityConverter = TaskEntityConverterImpl(gson = Gson())
+        taskEntityText = TaskEntity(
+            title = "something",
+            content = "context",
+            date = "date",
+            taskCategoryId = 4,
+            taskType = Task.TaskType.Text,
+            isPinned = true,
+            id = 32
+        )
+        taskEntityList = TaskEntity(
+            title = "something",
+            content = Gson().toJson(listOf<Task.TaskListItem>(
+                Task.TaskListItem(
+                    title = "some title",
+                    isChecked = true
+                )
+            )),
+            date = "date",
+            taskCategoryId = 4,
+            taskType = Task.TaskType.Text,
+            isPinned = true,
+            id = 32
+        )
+        taskText = Task.TaskText(
+            title = "something",
+            text = "context",
+            date = "date",
+            taskCategoryId = 4,
+            taskType = Task.TaskType.Text,
+            isPinned = true,
+            id = 32
+        )
+        taskList = Task.TaskList(
+            title = "something",
+            list = listOf<Task.TaskListItem>(
+                Task.TaskListItem(
+                    title = "some title",
+                    isChecked = true
+                )
+            ),
+            date = "date",
+            taskCategoryId = 4,
+            taskType = Task.TaskType.Text,
+            isPinned = true,
+            id = 32
+        )
+        listOfTaskEntityText = listOf(taskEntityText)
+        listOfTaskEntityList = listOf(taskEntityList)
+        listOfTaskText = listOf(taskText)
+        listOfTaskList = listOf(taskList)
+    }
+
+    @Test
+    fun test_convertTaskEntityToTaskText() {
+        val result: Task.TaskText = taskEntityConverter.convertTaskEntityToTaskText(taskEntity = taskEntityText)
+        assertEquals(true, result.title == taskEntityText.title)
+        assertEquals(true, result.text == taskEntityText.content)
+        assertEquals(true, result.date == taskEntityText.date)
+        assertEquals(true, result.taskCategoryId == taskEntityText.taskCategoryId)
+        assertEquals(true, result.taskType == taskEntityText.taskType)
+        assertEquals(true, result.isPinned == taskEntityText.isPinned)
+        assertEquals(true, result.id == taskEntityText.id)
+    }
+
+    @Test
+    fun test_convertTaskEntityToTaskList() {
+        val result: Task.TaskList = taskEntityConverter.convertTaskEntityToTaskList(taskEntity = taskEntityList)
+        assertEquals(true, result.title == taskEntityList.title)
+        assertEquals(true, result.list?.isNotEmpty())
+        assertEquals(true, result.list?.get(0)?.title == "some title")
+        assertEquals(true, result.list?.get(0)?.isChecked == true)
+        assertEquals(true, result.date == taskEntityList.date)
+        assertEquals(true, result.taskCategoryId == taskEntityList.taskCategoryId)
+        assertEquals(true, result.taskType == taskEntityList.taskType)
+        assertEquals(true, result.isPinned == taskEntityList.isPinned)
+        assertEquals(true, result.id == taskEntityList.id)
+    }
+
+    @Test
+    fun test_convertTaskTextToTaskEntity() {
+        val result: TaskEntity = taskEntityConverter.convertTaskTextToTaskEntity(task = taskText)
+        assertEquals(true, result.title == taskText.title)
+        assertEquals(true, result.content == taskText.text)
+        assertEquals(true, result.date == taskText.date)
+        assertEquals(true, result.taskCategoryId == taskText.taskCategoryId)
+        assertEquals(true, result.taskType == taskText.taskType)
+        assertEquals(true, result.isPinned == taskText.isPinned)
+        assertEquals(true, result.id == taskText.id)
+    }
+
+    @Test
+    fun test_convertTaskListToTaskEntity() {
+        val result: TaskEntity = taskEntityConverter.convertTaskListToTaskEntity(task = taskList)
+        assertEquals(true, result.title == taskList.title)
+        val list = Gson().fromJson(result.content, Array<Task.TaskListItem>::class.java).asList()
+        assertEquals(true, list == taskList.list)
+        assertEquals(true, result.date == taskList.date)
+        assertEquals(true, result.taskCategoryId == taskList.taskCategoryId)
+        assertEquals(true, result.taskType == taskList.taskType)
+        assertEquals(true, result.isPinned == taskList.isPinned)
+        assertEquals(true, result.id == taskList.id)
+    }
+
+    @Test
+    fun test_convertListOfTaskEntityToListOfTaskList() {
+        val result: Task.TaskList = (taskEntityConverter.convertListOfTaskEntityToListOfTaskList(list = listOfTaskEntityList).first())
+        assertEquals(true, result.title == taskEntityList.title)
+        assertEquals(true, result.list?.isNotEmpty())
+        assertEquals(true, result.list?.get(0)?.title == "some title")
+        assertEquals(true, result.list?.get(0)?.isChecked == true)
+        assertEquals(true, result.date == taskEntityList.date)
+        assertEquals(true, result.taskCategoryId == taskEntityList.taskCategoryId)
+        assertEquals(true, result.taskType == taskEntityList.taskType)
+        assertEquals(true, result.isPinned == taskEntityList.isPinned)
+        assertEquals(true, result.id == taskEntityList.id)
+    }
+
+    @Test
+    fun test_convertListOfTaskEntityToListOfTaskText() {
+        val result: Task.TaskText = (taskEntityConverter.convertListOfTaskEntityToListOfTaskText(list = listOfTaskEntityList).first())
+        assertEquals(true, result.title == taskEntityText.title)
+        assertEquals(true, result.text == taskEntityText.content)
+        assertEquals(true, result.date == taskEntityText.date)
+        assertEquals(true, result.taskCategoryId == taskEntityText.taskCategoryId)
+        assertEquals(true, result.taskType == taskEntityText.taskType)
+        assertEquals(true, result.isPinned == taskEntityText.isPinned)
+        assertEquals(true, result.id == taskEntityText.id)
+    }
+
+    @Test
+    fun test_convertListOfTaskTextToListOfTaskEntity() {
+        val result: TaskEntity = (taskEntityConverter.convertListOfTaskTextToListOfTaskEntity(listOfTaskText).first())
+        assertEquals(true, result.title == taskText.title)
+        assertEquals(true, result.content == taskText.text)
+        assertEquals(true, result.date == taskText.date)
+        assertEquals(true, result.taskCategoryId == taskText.taskCategoryId)
+        assertEquals(true, result.taskType == taskText.taskType)
+        assertEquals(true, result.isPinned == taskText.isPinned)
+        assertEquals(true, result.id == taskText.id)
+    }
+
+    @Test
+    fun test_convertListOfTaskListToListOfTaskEntity() {
+        val result: TaskEntity = (taskEntityConverter.convertListOfTaskListToListOfTaskEntity(list = listOfTaskList).first())
+        assertEquals(true, result.title == taskList.title)
+        val list = Gson().fromJson(result.content, Array<Task.TaskListItem>::class.java).asList()
+        assertEquals(true, list == taskList.list)
+        assertEquals(true, result.date == taskList.date)
+        assertEquals(true, result.taskCategoryId == taskList.taskCategoryId)
+        assertEquals(true, result.taskType == taskList.taskType)
+        assertEquals(true, result.isPinned == taskList.isPinned)
+        assertEquals(true, result.id == taskList.id)
+    }
 
 }
