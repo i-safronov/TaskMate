2341530461659429b41ac3f27b5482fd3bf6e1ec
safronov <ilya.safa1337w@gmail.com>
2023-10-30 13:10:12 +0500

    Added TaskService.kt

---
+++ data/src/main/java/safronov/apps/data/data_source/local/service/task/TaskService.kt
@@ -0,0 +1,17 @@
+package safronov.apps.data.data_source.local.service.task
+
+import kotlinx.coroutines.flow.Flow
+import safronov.apps.data.data_source.local.model.task.TaskEntity
+
+interface TaskService {
+
+    suspend fun insertTask(task: TaskEntity): Long?
+    suspend fun getTasksAsFlow(): Flow<List<TaskEntity>>
+    suspend fun getTasks(): List<TaskEntity>
+    suspend fun getTasksByText(text: String): List<TaskEntity>
+    suspend fun changeTask(task: TaskEntity)
+    suspend fun changeTasks(tasks: List<TaskEntity>)
+    suspend fun deleteTask(task: TaskEntity)
+    suspend fun deleteTasks(tasks: List<TaskEntity>)
+
+}
