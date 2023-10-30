3168e52319d09e417973157b3bd698d46d977f49
safronov <ilya.safa1337w@gmail.com>
2023-10-30 13:05:22 +0500

    Added TaskEntity::class in the entities in the AppDB.kt and changed db version

--- data/src/main/java/safronov/apps/data/data_source/local/app_db/core_sql/AppDB.kt
+++ data/src/main/java/safronov/apps/data/data_source/local/app_db/core_sql/AppDB.kt
@@ -3,12 +3,13 @@ package safronov.apps.data.data_source.local.app_db.core_sql
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import safronov.apps.data.data_source.local.app_db.dao_sql.TaskCategoryDao
+import safronov.apps.data.data_source.local.model.task.TaskEntity
 import safronov.apps.data.data_source.local.model.task_category.TaskCategoryEntity
 
 const val APP_DB_NAME = "TaskMateAppDB.db"
 
 @Database(
-    entities = [TaskCategoryEntity::class], version = 1
+    entities = [TaskCategoryEntity::class, TaskEntity::class], version = 2
 )
 abstract class AppDB(): RoomDatabase() {
     abstract fun getTaskCategoryDao(): TaskCategoryDao
