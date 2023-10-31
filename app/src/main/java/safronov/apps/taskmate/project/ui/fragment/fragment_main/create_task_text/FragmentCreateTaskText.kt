fca93844946335e383586fc011368b95a8bf4541
safronov <ilya.safa1337w@gmail.com>
2023-10-31 16:20:21 +0500

    Added injection FragmentCreateTaskTextViewModel in the FragmentCreateTaskText.kt

--- app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/create_task_text/FragmentCreateTaskText.kt
+++ app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/create_task_text/FragmentCreateTaskText.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import safronov.apps.taskmate.R
 import safronov.apps.taskmate.databinding.FragmentCreateTaskTextBinding
 import safronov.apps.taskmate.project.system_settings.extension.fragment.goToFragmentErrorFromHomePage
 import safronov.apps.taskmate.project.system_settings.extension.fragment.removeMenuFromHomePageToolBar
 import safronov.apps.taskmate.project.system_settings.extension.fragment.inflateMenuOnHomePageToolBar
+import safronov.apps.taskmate.project.system_settings.extension.fragment.requireAppComponent
 import safronov.apps.taskmate.project.system_settings.fragment.FragmentBase
+import safronov.apps.taskmate.project.ui.fragment.fragment_main.create_task_text.view_model.FragmentCreateTaskTextViewModel
+import safronov.apps.taskmate.project.ui.fragment.fragment_main.create_task_text.view_model.FragmentCreateTaskTextViewModelFactory
+import javax.inject.Inject
 
 class FragmentCreateTaskText : FragmentBase() {
 
     private var _binding: FragmentCreateTaskTextBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var fragmentCreateTaskTextViewModelFactory: FragmentCreateTaskTextViewModelFactory
+    private var fragmentCreateTaskTextViewModel: FragmentCreateTaskTextViewModel? = null
 
     override fun createUI(inflater: LayoutInflater, container: ViewGroup?): View? {
         _binding = FragmentCreateTaskTextBinding.inflate(inflater, container, false)
@@ -22,7 +31,9 @@ class FragmentCreateTaskText : FragmentBase() {
     }
 
     override fun setup() {
-
+        requireAppComponent().inject(this)
+        fragmentCreateTaskTextViewModel = ViewModelProvider(this, fragmentCreateTaskTextViewModelFactory)
+            .get(FragmentCreateTaskTextViewModel::class.java)
     }
 
     override fun uiCreated(view: View, savedInstanceState: Bundle?) {
