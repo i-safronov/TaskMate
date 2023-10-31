0ccc81b77c250049d32d189bb1d5821c65821b2d
safronov <ilya.safa1337w@gmail.com>
2023-10-31 15:31:18 +0500

    Added providers in the DataModule.kt for data module items

--- app/src/main/java/safronov/apps/taskmate/project/di/module/DataModule.kt
+++ app/src/main/java/safronov/apps/taskmate/project/di/module/DataModule.kt
@@ -3,19 +3,32 @@ package safronov.apps.taskmate.project.di.module
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.room.Room
+import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import safronov.apps.data.data_source.local.app_db.core_sql.APP_DB_NAME
 import safronov.apps.data.data_source.local.app_db.core_sql.AppDB
 import safronov.apps.data.data_source.local.app_db.dao_sql.TaskCategoryDao
+import safronov.apps.data.data_source.local.app_db.dao_sql.TaskDao
+import safronov.apps.data.data_source.local.model.converter.task.TaskEntityConverter
+import safronov.apps.data.data_source.local.model.converter.task.TaskEntityConverterImpl
+import safronov.apps.data.data_source.local.service.task.TaskService
+import safronov.apps.data.data_source.local.service.task.TaskServiceImpl
 import safronov.apps.data.data_source.local.service.task_category.TaskCategoryService
 import safronov.apps.data.data_source.local.service.task_category.TaskCategoryServiceImpl
 import safronov.apps.data.data_source.local.service.user_login.SharedPreferencesService
 import safronov.apps.data.data_source.local.service.user_login.SharedPreferencesServiceImpl
 import safronov.apps.data.data_source.local.service.user_login.SharedPreferencesServiceImpl.Companion.SHARED_PREFERENCES_NAME
+import safronov.apps.data.repository_impl.task.TaskRepositoryImpl
+import safronov.apps.domain.repository.task.TaskRepository
 
 @Module
 class DataModule {
+
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
 
     @Provides
     fun provideAppDB(context: Context): AppDB {
@@ -47,6 +60,23 @@ class DataModule {
         return SharedPreferencesServiceImpl(
             sharedPreferences = sharedPreferences
         )
+    }
+
+    @Provides
+    fun provideTaskDao(appDB: AppDB): TaskDao {
+        return appDB.getTaskDao()
+    }
+
+    @Provides
+    fun provideTaskService(taskDao: TaskDao): TaskService {
+        return TaskServiceImpl(
+            taskDao = taskDao
+        )
+    }
+
+    @Provides
+    fun provideTaskEntityConverter(gson: Gson): TaskEntityConverter {
+        return TaskEntityConverterImpl(gson = gson)
     }
 
 }
