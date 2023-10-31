9def4c614dd8037fe7991aa61fd1f24f18766efb
safronov <ilya.safa1337w@gmail.com>
2023-10-31 16:08:17 +0500

    Added inject for FragmentCreateTaskText and FragmentCreateTaskList in the AppComponent

--- app/src/main/java/safronov/apps/taskmate/project/di/component/AppComponent.kt
+++ app/src/main/java/safronov/apps/taskmate/project/di/component/AppComponent.kt
@@ -6,6 +6,9 @@ import safronov.apps.taskmate.project.di.module.DataModule
 import safronov.apps.taskmate.project.di.module.DomainModule
 import safronov.apps.taskmate.project.ui.activity.MainActivity
 import safronov.apps.taskmate.project.ui.fragment.fragment_main.FragmentMain
+import safronov.apps.taskmate.project.ui.fragment.fragment_main.create_task_list.FragmentCreateTaskList
+import safronov.apps.taskmate.project.ui.fragment.fragment_main.create_task_text.FragmentCreateTaskText
+import safronov.apps.taskmate.project.ui.fragment.fragment_main.create_task_text.view_model.FragmentCreateTaskTextViewModelFactory
 import safronov.apps.taskmate.project.ui.fragment.start.FragmentStart
 import safronov.apps.taskmate.project.ui.fragment.welcome.FragmentWelcome
 import javax.inject.Singleton
@@ -18,5 +21,7 @@ interface AppComponent {
     fun inject(fragmentStart: FragmentStart)
     fun inject(fragmentWelcome: FragmentWelcome)
     fun inject(fragmentMain: FragmentMain)
+    fun inject(fragmentCreateTaskText: FragmentCreateTaskText)
+    fun inject(fragmentCreateTaskList: FragmentCreateTaskList)
 
 }
