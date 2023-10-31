6a79575b117d2bcea0a775ec4a92863182df2b0e
safronov <ilya.safa1337w@gmail.com>
2023-10-31 15:50:54 +0500

    Added providers in the DomainModule.kt

--- app/src/main/java/safronov/apps/taskmate/project/di/module/DomainModule.kt
+++ app/src/main/java/safronov/apps/taskmate/project/di/module/DomainModule.kt
@@ -2,12 +2,27 @@ package safronov.apps.taskmate.project.di.module
 
 import dagger.Module
 import dagger.Provides
+import safronov.apps.data.data_source.local.model.converter.task.TaskEntityConverter
+import safronov.apps.data.data_source.local.service.task.TaskService
 import safronov.apps.data.data_source.local.service.task_category.TaskCategoryService
 import safronov.apps.data.data_source.local.service.user_login.SharedPreferencesService
+import safronov.apps.data.repository_impl.task.TaskRepositoryImpl
 import safronov.apps.data.repository_impl.task_category.TaskCategoryRepositoryImpl
 import safronov.apps.data.repository_impl.user_login.UserLoginRepositoryImpl
+import safronov.apps.domain.repository.task.TaskRepository
 import safronov.apps.domain.repository.task_category.TaskCategoryRepository
 import safronov.apps.domain.repository.user_login.UserLoginRepository
+import safronov.apps.domain.use_case.task.create.InsertTaskListUseCase
+import safronov.apps.domain.use_case.task.create.InsertTaskTextUseCase
+import safronov.apps.domain.use_case.task.delete.DeleteTaskListUseCase
+import safronov.apps.domain.use_case.task.delete.DeleteTaskTextUseCase
+import safronov.apps.domain.use_case.task.delete.DeleteTasksUseCase
+import safronov.apps.domain.use_case.task.read.GetTasksAsFlowUseCase
+import safronov.apps.domain.use_case.task.read.GetTasksByTextUseCase
+import safronov.apps.domain.use_case.task.read.GetTasksUseCase
+import safronov.apps.domain.use_case.task.update.ChangeTaskListUseCase
+import safronov.apps.domain.use_case.task.update.ChangeTaskTextUseCase
+import safronov.apps.domain.use_case.task.update.ChangeTasksUseCase
 import safronov.apps.domain.use_case.task_category.create.InsertTaskCategoriesUseCase
 import safronov.apps.domain.use_case.task_category.read.GetTaskCategoriesUseCase
 import safronov.apps.domain.use_case.task_category.read.GetTaskCategoryByIdUseCase
@@ -90,6 +105,94 @@ class DomainModule {
         return IsUserLoggedInUseCase(
             userLoginRepository = userLoginRepository
         )
+    }
+
+    @Provides
+    fun provideTaskRepository(
+        taskService: TaskService,
+        taskEntityConverter: TaskEntityConverter
+    ): TaskRepository.TaskRepositoryMutable {
+        return TaskRepositoryImpl(
+            taskService = taskService,
+            taskEntityConverter = taskEntityConverter
+        )
+    }
+
+    @Provides
+    fun provideInsertTaskListUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): InsertTaskListUseCase {
+        return InsertTaskListUseCase(insertingTask = taskRepository)
+    }
+
+    @Provides
+    fun provideInsertTaskTextUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): InsertTaskTextUseCase {
+        return InsertTaskTextUseCase(insertingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideDeleteTaskListUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): DeleteTaskListUseCase {
+        return DeleteTaskListUseCase(deletingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideDeleteTasksUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): DeleteTasksUseCase {
+        return DeleteTasksUseCase(deletingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideDeleteTaskTextUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): DeleteTaskTextUseCase {
+        return DeleteTaskTextUseCase(deletingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideGetTasksAsFlowUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): GetTasksAsFlowUseCase {
+        return GetTasksAsFlowUseCase(gettingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideGetTasksByTextUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): GetTasksByTextUseCase {
+        return GetTasksByTextUseCase(gettingTasksByParametersRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideGetTasksUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): GetTasksUseCase {
+        return GetTasksUseCase(gettingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideChangeTaskListUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): ChangeTaskListUseCase {
+        return ChangeTaskListUseCase(changingTaskRepository = taskRepository)
+    }
+
+    @Provides
+    fun provideChangeTasksUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): ChangeTasksUseCase {
+        return ChangeTasksUseCase(changingTasksUseCase = taskRepository)
+    }
+
+    @Provides
+    fun provideChangeTaskTextUseCase(
+        taskRepository: TaskRepository.TaskRepositoryMutable
+    ): ChangeTaskTextUseCase {
+        return ChangeTaskTextUseCase(changingTaskRepository = taskRepository)
     }
 
 }
