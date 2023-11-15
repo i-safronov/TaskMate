cf65f8730e4e6f1c2e0e0c4e555e4ec3b344d468
i-safronov <ilya.safa1337w@gmail.com>
2023-11-15 14:21:18 +0500

    Added RcvChangingTaskCategory.kt

---
+++ app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/rcv/task_category/changing/RcvChangingTaskCategory.kt
@@ -0,0 +1,58 @@
+package safronov.apps.taskmate.project.ui.fragment.fragment_main.rcv.task_category.changing
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import safronov.apps.domain.model.task_category.TaskCategory
+import safronov.apps.taskmate.R
+import safronov.apps.taskmate.databinding.RcvItemTaskCategoryBinding
+
+class RcvChangingTaskCategory(): RecyclerView.Adapter<RcvChangingTaskCategory.TaskCategoryViewHolder>() {
+
+    private var categories = listOf<TaskCategory>()
+    private var selectedTaskCategory: TaskCategory? = null
+
+    inner class TaskCategoryViewHolder(
+        private val binding: RcvItemTaskCategoryBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bindView(item: TaskCategory) {
+            binding.img.setImageResource(item.icon ?: R.drawable.ic_block)
+            binding.tvTitle.text = item.categoryName
+            binding.imgIsPinned.visibility =
+                if (item.id == selectedTaskCategory?.id) View.VISIBLE else View.GONE
+            itemView.setOnClickListener {
+                saveSelectedItem(item)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskCategoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = RcvItemTaskCategoryBinding.inflate(inflater, parent, false)
+        return TaskCategoryViewHolder(binding = binding)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    override fun onBindViewHolder(holder: TaskCategoryViewHolder, position: Int) {
+        holder.bindView(categories[position])
+    }
+
+    fun submitList(list: List<TaskCategory>) {
+        categories = list
+        notifyDataSetChanged()
+    }
+
+    fun setSelectedTaskCategory(item: TaskCategory) {
+        saveSelectedItem(item)
+    }
+
+    private fun saveSelectedItem(item: TaskCategory) {
+        selectedTaskCategory = item
+        notifyDataSetChanged()
+    }
+
+}
