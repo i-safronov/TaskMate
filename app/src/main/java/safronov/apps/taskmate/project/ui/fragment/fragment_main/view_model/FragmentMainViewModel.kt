02f37fe1313d4c39685f3a7e270fe1832de2c0b0
i-safronov <ilya.safa1337w@gmail.com>
2023-11-16 14:14:26 +0500

    Implemented updateTaskCategories method in the FragmentMainViewModel.kt

--- app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/view_model/FragmentMainViewModel.kt
+++ app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/view_model/FragmentMainViewModel.kt
@@ -11,6 +11,7 @@ import safronov.apps.domain.model.task_category.TaskCategory
 import safronov.apps.domain.use_case.task.delete.DeleteTasksUseCase
 import safronov.apps.domain.use_case.task.read.GetTasksAsFlowUseCase
 import safronov.apps.domain.use_case.task_category.read.GetTaskCategoriesUseCase
+import safronov.apps.domain.use_case.task_category.update.UpdateTaskCategoriesUseCase
 import safronov.apps.taskmate.project.system_settings.coroutines.DispatchersList
 import safronov.apps.taskmate.project.system_settings.view_model.BaseViewModelImpl
 
@@ -18,7 +19,8 @@ class FragmentMainViewModel(
      private val dispatchersList: DispatchersList,
      private val getTasksAsFlowUseCase: GetTasksAsFlowUseCase,
      private val deleteTasksUseCase: DeleteTasksUseCase,
-     private val getTaskCategoriesUseCase: GetTaskCategoriesUseCase
+     private val getTaskCategoriesUseCase: GetTaskCategoriesUseCase,
+     private val updateTaskCategoriesUseCase: UpdateTaskCategoriesUseCase
 ): BaseViewModelImpl(dispatchersList = dispatchersList) {
 
     private val _isLoading = MutableStateFlow<Boolean?>(null)
@@ -58,6 +60,17 @@ class FragmentMainViewModel(
                 }
             }, showUi = { },
             wasException = {
+                _isException.value = it
+            }
+        )
+    }
+
+    fun updateTaskCategories(categories: List<TaskCategory>) {
+        asyncWork(
+            showUiWorkStarted = {},
+            doWork = {
+                updateTaskCategoriesUseCase.execute(categories)
+            }, showUi = {}, wasException = {
                 _isException.value = it
             }
         )
