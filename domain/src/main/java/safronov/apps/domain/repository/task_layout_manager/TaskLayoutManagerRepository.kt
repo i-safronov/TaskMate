c3711ba8b5f40036dcd1a8f8aaf9cd9dd727bcec
i-safronov <ilya.safa1337w@gmail.com>
2023-11-17 14:38:50 +0500

    Added TaskLayoutManagerRepository.kt

---
+++ domain/src/main/java/safronov/apps/domain/repository/task_layout_manager/TaskLayoutManagerRepository.kt
@@ -0,0 +1,15 @@
+package safronov.apps.domain.repository.task_layout_manager
+
+import safronov.apps.domain.model.task_layout_manager.TaskLayoutManager
+
+interface TaskLayoutManagerRepository {
+
+    interface SavingTaskLayoutManagerRepository {
+        suspend fun saveTaskLayoutManager(manager: TaskLayoutManager)
+    }
+
+    interface GettingTaskLayoutManager {
+        suspend fun getTaskLayoutManager(): String?
+    }
+
+}
