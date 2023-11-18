93b29da357b896df2506e6226d3b009aaa3a91d6
i-safronov <ilya.safa1337w@gmail.com>
2023-11-18 10:10:17 +0500

    Changed FragmentMainViewModel.kt to load task layout manager

--- app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/view_model/FragmentMainViewModel.kt
+++ app/src/main/java/safronov/apps/taskmate/project/ui/fragment/fragment_main/view_model/FragmentMainViewModel.kt
@@ -7,10 +7,13 @@ import kotlinx.coroutines.withContext
 import safronov.apps.domain.exception.DomainException
 import safronov.apps.domain.model.task.Task
 import safronov.apps.domain.model.task_category.TaskCategory
+import safronov.apps.domain.model.task_layout_manager.TaskLayoutManager
 import safronov.apps.domain.use_case.task.delete.DeleteTasksUseCase
 import safronov.apps.domain.use_case.task.read.GetTasksAsFlowByTaskCategoryUseCase
 import safronov.apps.domain.use_case.task_category.read.GetTaskCategoriesUseCase
 import safronov.apps.domain.use_case.task_category.update.UpdateTaskCategoriesUseCase
+import safronov.apps.domain.use_case.task_layout_manager.GetTaskLayoutManagerUseCase
+import safronov.apps.domain.use_case.task_layout_manager.SaveTaskLayoutManagerUseCase
 import safronov.apps.taskmate.project.system_settings.coroutines.DispatchersList
 import safronov.apps.taskmate.project.system_settings.view_model.BaseViewModelImpl
 
@@ -19,7 +22,9 @@ class FragmentMainViewModel(
      private val deleteTasksUseCase: DeleteTasksUseCase,
      private val getTaskCategoriesUseCase: GetTaskCategoriesUseCase,
      private val updateTaskCategoriesUseCase: UpdateTaskCategoriesUseCase,
-     private val getTasksAsFlowByTaskCategoryUseCase: GetTasksAsFlowByTaskCategoryUseCase
+     private val getTasksAsFlowByTaskCategoryUseCase: GetTasksAsFlowByTaskCategoryUseCase,
+     private val getTaskLayoutManagerUseCase: GetTaskLayoutManagerUseCase,
+     private val saveTaskLayoutManagerUseCase: SaveTaskLayoutManagerUseCase
 ): BaseViewModelImpl(dispatchersList = dispatchersList) {
 
     private val _isLoading = MutableStateFlow<Boolean?>(null)
@@ -27,12 +32,14 @@ class FragmentMainViewModel(
     private val _isException = MutableStateFlow<DomainException?>(null)
     private val _taskCategories = MutableStateFlow<List<TaskCategory>>(emptyList())
     private val _taskCategory = MutableStateFlow<TaskCategory?>(null)
+    private val _taskLayoutManager = MutableStateFlow<TaskLayoutManager?>(null)
 
     fun getIsLoading(): StateFlow<Boolean?> = _isLoading
     fun getTasks(): StateFlow<List<Task>?> = _tasks
     fun getIsWasException(): StateFlow<DomainException?> = _isException
     fun getCategories(): StateFlow<List<TaskCategory>> = _taskCategories
     fun getCategory(): StateFlow<TaskCategory?> = _taskCategory
+    fun getTaskLayoutManager(): StateFlow<TaskLayoutManager?> = _taskLayoutManager
 
     fun whichFragmentToGoByTaskType(
         taskType: Task.TaskType,
@@ -46,7 +53,7 @@ class FragmentMainViewModel(
         }
     }
 
-    fun loadTasks() {
+    fun loadPage() {
         asyncWork(
             showUiWorkStarted = {
                 _isLoading.value = true
@@ -56,6 +63,7 @@ class FragmentMainViewModel(
                 if (_taskCategory.value == null && _taskCategories.value.isNotEmpty()) {
                     _taskCategory.value = _taskCategories.value.first()
                 }
+                _taskLayoutManager.value = getTaskLayoutManagerUseCase.execute()
                 getTasksAsFlowByTaskCategoryUseCase.execute(
                     taskCategory = _taskCategory.value ?: throw DomainException("couldn't found task category")
                 ).collect {
@@ -73,7 +81,7 @@ class FragmentMainViewModel(
 
     fun reloadTasksByTaskCategory(category: TaskCategory) {
         _taskCategory.value = category
-        loadTasks()
+        loadPage()
     }
 
     fun updateTaskCategories(categories: List<TaskCategory>) {
@@ -90,7 +98,17 @@ class FragmentMainViewModel(
     fun deleteTasks(deleted: List<Task>) {
         asyncWork(showUiWorkStarted = {}, doWork = {
             deleteTasksUseCase.execute(deleted)
-        }, showUi = { loadTasks() }, wasException = {
+        }, showUi = { loadPage() }, wasException = {
+            _isException.value = it
+        })
+    }
+
+    fun saveTaskLayoutManagerUseCase(
+        manager: TaskLayoutManager
+    ) {
+        asyncWork(showUiWorkStarted = {}, doWork = {
+            saveTaskLayoutManagerUseCase.execute(manager)
+        }, showUi = {}, wasException = {
             _isException.value = it
         })
     }
